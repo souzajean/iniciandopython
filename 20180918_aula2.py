77b541599e216c4ab271f377fab45cc99092d2a0
Jean Souza <43140051+souzajean@users.noreply.github.com>
2018-09-20 22:23:16 -0300

    Add files via upload

---
+++ 20180918_aula2.py
@@ -0,0 +1,63 @@
+#Version: Python 3.6 -> data:18/09/2018
+
+#Operadores Aritméticos
+
+#variavel
+a=10
+b=2
+
+#soma
+resultadosoma=(a+b)
+print('resultado soma a+b =',resultadosoma)
+
+#divisao
+resulatdodivisao=(a/b)
+resultadodivisao1=(b/a)
+print('resultado divisao a/b =',resulatdodivisao)
+print('resultado divisao b/a =',resultadodivisao1)
+
+#multiplicacao
+resultadomultiplicacao=(a*b)
+print('resultado multiplicacao a*b =',resultadomultiplicacao)
+
+#exponenciacao
+#2**3 = 8 # É o mesmo que dois ao cubo
+#2**(3+6) = 512 # Dois elevado a nove
+resultadoexponenciacao=(a**b)
+print('resultado exponenciacao a**b =',resultadoexponenciacao)
+
+#modulo
+
+resultadomodulo=(a%b)
+resultadomodulo1=(b%a)
+print('O resto da divisão entre a%b =',resultadomodulo)
+print('O resto da divisão entre b%a =',resultadomodulo1)
+
+
+#Atribuindo um valor para varias variaveis
+x = y = z = 100
+print('x =',x)
+print('y =',y)
+print('z =',z)
+
+
+#Operadores de atribuição
+
+#+= Soma com valor atribuído
+a+=b # a=a+b
+print('Soma com valor atribuído a =',a)
+
+#-= Subtrai o valor atribuído
+a-=b #a=a-b
+print('Subtrai o valor atribuído a =',a)
+
+#/= Divide pelo valor atribuído
+a/=b
+print('Divide pelo valor atribuído a =',a)
+
+#*= Multiplica pelo valor atribuído
+a*=b
+print('Multiplica pelo valor atribuído a =',a)
+
+
+
