4b5b95e9d534ea306a7a21049404408541e45bf8
Jean Souza <43140051+souzajean@users.noreply.github.com>
2018-09-21 23:10:35 -0300

    Add files via upload

---
+++ 20180920_aula4.py
@@ -0,0 +1,52 @@
+#Version: Python 3.6 -> data:20/09/2018
+
+#Operadores aritimeticos
+
+#x + y (adição),
+#x - y (subtração),
+#x / y (divisão em ponto flutuante),
+#x// y (divisão descartando parte fracionária),
+#x * y , (multiplicação),
+#x % y (resto),
+#x (negação)
+#x ** y (potência)
+
+
+print(1+2)
+print(3-1)
+print(10/2)
+print(10//3)
+print(10*2+1)
+print(10%3)
+print(-3)
+print(2**8)
+
+
+#Operadores de bits
+
+#x | y (ou),
+#x ^y (ou exclusivo),
+#x & y (e),
+#x << y (x com y bits deslocados à esquerda),
+#x >> y (x com y bits deslocados à direita)
+#~x (inverso em bits).
+
+print('Operadores de bits: 1 | 0 (ou)', 1 | 0)
+print('Operadores de bits: 2 | 5 (ou)', 2 | 5)
+print('Operadores de bits: 1 ^5 (ou exclusivo)',1^5)
+print('Operadores de bits: 4 & 1 (e)',4&1)
+print('Operadores de bits: x << y (x com y bits deslocados à esquerda)',1<<2)
+print('Operadores de bits: 4 >> 2 (x com y bits deslocados à direita)',2>>4)
+print('Operadores de bits: ~4 (inverso em bits)',~4)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 20180921_aula5.py
@@ -0,0 +1,36 @@
+#Version: Python 3.6 -> data:21/09/2018
+
+#Operadores condição
+
+#pagina 19
+#= = Igual
+#! = Diferente
+#> Maior
+#< Menor
+#>= Maior ou igual
+#<= Menor ou igual
+
+print()
+print('Igual:102 == 102',102 == 102)
+print('Igual: 1 == 1.0', 1==1.0)
+print('Igual: 21 == 12',21 == 12)
+print()
+print('Diferente: 9!=8',9!=8)
+print('Diferente: 8!=9',8!=9)
+print('Diferente: 1!=1',1!=1)
+print()
+print('Maior: 5 > 6',5>6)
+print('Maior: 6 > 7',5>6)
+print('Maior: 6 > 6',5>6)
+print()
+print('Menor: 2 < 1',2 < 1)
+print('Menor: 1 < 2',1 < 2)
+print('Menor: 7 < 7',7 < 7)
+print()
+print('Maior ou igual: 1 >= 1',1 >=1)
+print('Maior ou igual: 2 >= 1',2 >=1)
+print('Maior ou igual: 1 >= 2',1 >=2)
+print()
+print('Menor ou igual: 2 <=3',2 <=3)
+print('Menor ou igual: 3 <=2',3 <=2)
+print('Menor ou igual: 3 <=3',3 <=3)
